f0231d9a611a531ff827dfe12d4e7b8a74d12170
Ayesha Mukhtar Asad <168007496+Ayesha-Asadd@users.noreply.github.com>
2025-04-12 21:21:03 +0500

    Contain ui with crud of Manage Task

--- SmartTrackWeb/src/App.jsx
+++ SmartTrackWeb/src/App.jsx
@@ -1,11 +1,13 @@
 import DashBoard from "./pages/DashBoard";
 import Calender from "./pages/Calender";
-import Task from "./pages/Task";
+import TaskManage from "./pages/Task";
 import Journal from "./pages/Journal";
 function App() {
   return (
     <>
+  
       <DashBoard></DashBoard>
+      <TaskManage></TaskManage>
     </>
   );
 }
