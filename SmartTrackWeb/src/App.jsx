b1ff25db5ffce93cd91cdb62483875f79c5e657b
Jaweriya khan <JAWERIYAKHAN26>
2025-04-13 13:13:28 -0400

    Made UI changes and added pages

--- SmartTrackWeb/src/App.jsx
+++ SmartTrackWeb/src/App.jsx
@@ -2,6 +2,9 @@ import DashBoard from "./pages/DashBoard";
 import Calender from "./pages/Calender";
 import Task from "./pages/Task";
 import Journal from "./pages/Journal";
+import JournalEntries from "./pages/JournalEntries";
+import JournalForm from "./pages/JournalForm";
+import Sidebar from "./pages/Sidebar";
 function App() {
   return (
     <>
